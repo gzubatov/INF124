6da5e43699009206593478294144aad6301fa340
Greg Zubatov <gzubatov@gmail.com>
2020-04-13 16:24:28 -0700

    fix formatting

--- js/products.js
+++ js/products.js
@@ -11,9 +11,9 @@ function run() {
 		const phoneNum = document.querySelector('#phonenum').value;
 
 		verifyQuantity(qty);
-        verifyName(firstName, lastName);
-        verifyPhoneNumber(phoneNum);    
-        verifyAddress(shipAddress);
+		verifyName(firstName, lastName);
+		verifyPhoneNumber(phoneNum);
+		verifyAddress(shipAddress);
 		verifyCreditCard(creditCardNum, ccv);
 	};
 
@@ -25,49 +25,49 @@ function run() {
 Functions to verify the form input
 */
 function verifyQuantity(qty) {
-    if (qty.length === 0) {
-        alert('Must enter a quanity');
-        event.preventDefault();
-    }
-    else if (isNaN(qty) === false && parseInt(qty) <= 0) {
-        alert('Cannot enter Quantity Value below 1');
-        event.preventDefault();
-    }
+	if (qty.length === 0) {
+		alert('Must enter a quanity');
+		event.preventDefault();
+	}
+	else if (isNaN(qty) === false && parseInt(qty) <= 0) {
+		alert('Cannot enter Quantity Value below 1');
+		event.preventDefault();
+	}
 }
 
 function verifyName(firstName, lastName) {
-    if (firstName === '') {
-        alert('First Name Field is empty');
-        event.preventDefault();
-    }
-    if (lastName === '') {
-        alert('Last Name Field is empty');
-        event.preventDefault();
-    }
+	if (firstName === '') {
+		alert('First Name Field is empty');
+		event.preventDefault();
+	}
+	if (lastName === '') {
+		alert('Last Name Field is empty');
+		event.preventDefault();
+	}
 }
 
 function verifyPhoneNumber(phoneNum) {
-    if (phoneNum === '') {
-        alert('Phone Number Field is empty');
-        event.preventDefault();
-    }
+	if (phoneNum === '') {
+		alert('Phone Number Field is empty');
+		event.preventDefault();
+	}
 }
 
 function verifyAddress(shipAddress) {
-    if (shipAddress === '') {
-        alert('Shipping Address Field is empty');
-        event.preventDefault();
-    }
+	if (shipAddress === '') {
+		alert('Shipping Address Field is empty');
+		event.preventDefault();
+	}
 }
 
 function verifyCreditCard(creditCardNum, ccv) {
-    if (isNaN(creditCardNum) === false && creditCardNum.length !== 16) {
-        alert('Credit card number is invalid. Should be 16 digits.');
-        event.preventDefault();
-    }
+	if (isNaN(creditCardNum) === false && creditCardNum.length !== 16) {
+		alert('Credit card number is invalid. Should be 16 digits.');
+		event.preventDefault();
+	}
 
-    if (isNaN(ccv) === false && ccv.length !== 3) {
-        alert('CCV is invalid. Should be 3 digits.');
-        event.preventDefault();
-    }
+	if (isNaN(ccv) === false && ccv.length !== 3) {
+		alert('CCV is invalid. Should be 3 digits.');
+		event.preventDefault();
+	}
 }
