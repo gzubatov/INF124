d0e15af590681a835b7f30d58969b2f6585692db
Greg Zubatov <gzubatov@gmail.com>
2020-04-13 15:24:13 -0700

    Add product page

---
+++ js/products.js
@@ -0,0 +1,49 @@
+window.onload = run;
+
+function run() {
+	const onFormSubmit = (event) => {
+		console.log('swan');
+		const qty = document.querySelector('#quantity').value;
+		const firstName = document.querySelector('#fname').value;
+		const lastName = document.querySelector('#lname').value;
+		const shipAddress = document.querySelector('#addr').value;
+		const creditCardNum = document.querySelector('#ccn').value;
+		const ccv = document.querySelector('#security').value;
+		const phone = document.querySelector('#phoneNum').value;
+
+		if (isNaN(qty) === false && parseInt(qty) <= 0) {
+			alert('Cannot enter Quantity Value below 1');
+			event.preventDefault();
+		}
+
+		if (firstName === '') {
+			alert('First Name Field is empty');
+			event.preventDefault();
+		}
+		if (lastName === '') {
+			alert('Last Name Field is empty');
+			event.preventDefault();
+		}
+		if (phoneNum === '') {
+			alert('Phone Number Field is empty');
+			event.preventDefault();
+		}
+		if (shipAddress === '') {
+			alert('Shipping Address Field is empty');
+			event.preventDefault();
+		}
+
+		if (isNaN(creditCardNum) === false && creditCardNum.length !== 16) {
+			alert('Credit card number is invalid. Should be 16 digits.');
+			event.preventDefault();
+		}
+
+		if (isNaN(ccv) === false && ccv.length !== 3) {
+			alert('CCV is invalid. Should be 3 digits.');
+			event.preventDefault();
+		}
+	};
+
+	const form = document.querySelector('form');
+	form.addEventListener('submit', onFormSubmit);
+}
