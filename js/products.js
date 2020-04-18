e80249b4d1659f93cabcc4c342097c58edc62e04
Greg Zubatov <gzubatov@gmail.com>
2020-04-18 15:59:14 -0700

    Finish order form, product pages, home page styling

--- js/products.js
+++ js/products.js
@@ -4,6 +4,7 @@ let errors = '';
 
 function run() {
 	const onFormSubmit = (event) => {
+		const identifier = document.querySelector('#prod_id').value;
 		const qty = document.querySelector('#quantity').value;
 		const firstName = document.querySelector('#fname').value;
 		const lastName = document.querySelector('#lname').value;
@@ -11,11 +12,14 @@ function run() {
 		const creditCardNum = document.querySelector('#ccn').value;
 		const ccv = document.querySelector('#security').value;
 		const phoneNum = document.querySelector('#phonenum').value;
-
+		const city = document.querySelector('#city').value;
+		const zip = document.querySelector("#zipcode").value;
+		
+		verifyIdentifier(identifier);
 		verifyQuantity(qty);
 		verifyName(firstName, lastName);
 		verifyPhoneNumber(phoneNum);
-		verifyAddress(shipAddress);
+		verifyAddress(shipAddress, city, zip);
 		verifyCreditCard(creditCardNum, ccv);
 
 		if (errors !== '') {
@@ -31,11 +35,18 @@ function run() {
 /*
 Functions to verify the form input
 */
+function verifyIdentifier(identifier) {
+	if(identifier === '')
+	{
+		errors += 'Product Identifier field is empty\n';
+	}
+}
+
 function verifyQuantity(qty) {
 	if (qty.length === 0) {
 		errors += 'Must enter a quantity\n';
 	}
-	else if (isNaN(qty) === false && parseInt(qty) <= 0) {
+	else if (isNaN(qty) || parseInt(qty) <= 0) {
 		errors += 'Cannot enter Quantity Value below 1\n';
 	}
 }
@@ -55,18 +66,24 @@ function verifyPhoneNumber(phoneNum) {
 	}
 }
 
-function verifyAddress(shipAddress) {
+function verifyAddress(shipAddress, city, zip) {
 	if (shipAddress === '') {
 		errors += 'Shipping Address Field is empty\n';
+	}
+	if (city === '') {
+		errors += 'City cannot be empty\n';
+	}
+	if (isNaN(zip) || zip.length !== 5) {
+		errors += 'Zip code is not valid\n';
 	}
 }
 
 function verifyCreditCard(creditCardNum, ccv) {
-	if (isNaN(creditCardNum) === false && creditCardNum.length !== 16) {
+	if (isNaN(creditCardNum) || creditCardNum.length !== 16) {
 		errors += 'Credit card number is invalid. Should be 16 digits.\n';
 	}
 
-	if (isNaN(ccv) === false && ccv.length !== 3) {
+	if (isNaN(ccv) || ccv.length !== 3) {
 		errors += 'CCV is invalid. Should be 3 digits.\n';
 	}
 }
